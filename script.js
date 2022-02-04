51cda00f1d1b7ea315169950ac98d764c16ac09a
gitaengg <97780987+gitaengg@users.noreply.github.com>
2022-02-04 20:08:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ankita Ji" && b=="05-02-1999")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
